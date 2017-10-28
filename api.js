e13231278221c41ff85590b1f8a17d35e3e9efad
mjhd <mjhd@devlion.net>
2017-10-28 14:27:35 +0900

    fix cache

--- api.js
+++ api.js
@@ -1,6 +1,11 @@
 
 function getTimetable(){
-  $.getJSON("http://localhost:8080/timetable", function( data ) {
+  $.ajax({
+    url: "http://localhost:8080/timetable",
+    dataType: "json",
+    type: "GET",
+    ifModified: true
+  }, function(data) {
     console.log(data);
   });
 }
